3d76fedb0acfacdfdf7da2c4cdea1481a7d21172
Bryan <btbonval@gmail.com>
2014-06-24 21:55:34 -0400

    using bootstrap 3 recipe for tray but its an ugly hack

--- annotorious.js
+++ annotorious.js
@@ -10,10 +10,10 @@ if (Meteor.isClient) {
    */
 
   /*
-   Some project configuration settings for flexibility.
+   Welcome Header
    */
   Template.welcome_header.project = function () {
-    /* Pull this from description.js */
+    /* TODO Pull this from description.js or the data store */
     var description = {
       'title': 'Annotorious'
     };
@@ -57,7 +57,6 @@ if (Meteor.isClient) {
    */
   Template.anonymous_browse.events({
     'click #anonymous_browse_link': function () {
-      console.log('CLICKED!!!');
       Router.go('/library');
     }
   });
@@ -77,6 +76,46 @@ if (Meteor.isClient) {
   /*
    LIBRARY TEMPLATE
    */
+
+
+  /*
+   TOP PANEL TEMPLATE
+   */
+
+  Template.top_bar.events({
+    'click [data-toggle=offcanvas]': function () {
+      /*
+       TODO: look into LESS to alias classes.
+       http://www.miuaiga.com/index.cfm/2013/11/2/Dynamic-Bootstrap-Layout-on-any-markup-with-makecolumn-and-makerow-via-LESS
+       e.g. in big mode, sidebar class would map to "col-xs-6 col-sm-3"
+            which means content class maps to "col-xs-6 col-sm-9"
+            in small mode, inactive, "sidebar" isn't there
+            which means content class maps to "col-xs-12"
+            in small mode, active, "sidebar" maps to "col-xs-6 col-sm-3"
+            which means content class maps to "col-xs-6 col-sm-9"
+
+       unsure how to tell big mode / small mode without a media check
+       which is independent of bootstrap.
+
+       it's impossible to effectively adjust columns?
+       presently, the col-xs-6 and col-sm-9 on content cause undesireable
+       wrapping behavior due to the hack used to pull the tray off the side.
+
+       maybe best bet is to $.addClass and $.removeClass against the 
+       sidebar and content divs right here, since this button is only
+       available in small mode anyway. very ugly solution :(
+       */
+      $('.row-offcanvas').toggleClass('active');
+    }
+  });
+
+  Template.top_bar.project = function () {
+    /* TODO Pull this from description.js or the data store */
+    var description = {
+      'title': 'Annotorious'
+    };
+    return description.title;
+  };
 
 }
 
