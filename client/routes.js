eaee05e1c9fe37a0e12862e80a2b3c56c45724c4
Bryan <btbonval@gmail.com>
2014-06-26 13:49:22 -0400

    using templatelayouts for header and tray.

--- client/routes.js
+++ client/routes.js
@@ -3,15 +3,27 @@ Router.map(function() {
   // Formatted as { '/path': 'template', ... }
   // This seems nicer in a config file than hard coding it here.
   // Pull this from routes.js
-  var routes = {
+  var straight_routes = {
     "/": "welcome",
     "/welcome": "welcome",
+  };
+  var top_and_side_routes = {
     "/about": "about",
     "/library": "library"
   };
-  var paths = Object.keys(routes)
+
+  var paths = Object.keys(straight_routes);
   for (var idx in paths) {
-    var key = paths[idx]
-    this.route(routes[key], {path: key});
+    var key = paths[idx];
+    this.route(straight_routes[key], {path: key});
+  }
+
+  var paths = Object.keys(top_and_side_routes);
+  for (var idx in paths) {
+    var key = paths[idx];
+    this.route(top_and_side_routes[key], {
+        path: key,
+        layoutTemplate: 'top_and_side'
+    });
   }
 });
