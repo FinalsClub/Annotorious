04df11568843cf7e3e66ae7abf11bd764de53325
Bryan <btbonval@gmail.com>
2014-07-10 01:10:19 -0400

    added sorting of library works on arbitrary fields

--- client/controllers/library.js
+++ client/controllers/library.js
@@ -1,8 +1,33 @@
 if (Meteor.isClient) {
 
   var get_works = function() {
-    /* TODO support {sort: [blah]} */
-    return Works.find({}, { fields: { title: 1, author: 1, summary: 1 } });
+    var query_opts = {};
+    var find_opts = { fields: { title: 1, author: 1, summary: 1 } };
+    var work_array = Works.find(query_opts, find_opts).map(modify_works);
+    /* have to switch to long form due to variable key */
+    return work_array.sort(sort_works);
+  }
+
+  /* helper function for changing works returned */
+  var modify_works = function(work, id, cursor) {
+    /* TODO
+    work.annotation_count = some_function(work.id);
+     */
+    return work;
+  }
+
+  /* helper function for sorting works */
+  var sort_works = function(work1, work2) {
+    Session.setDefault("library_sort_attr", "title");
+    var attr = Session.get("library_sort_attr")
+    /* test if both items in the list are the same */
+    if (work1[attr] == work2[attr]) return 0;
+    /* outsource sorting and figure out what it means */
+    var outsource = [work1[attr], work2[attr]].sort()
+    /* order maintained implies negative ordering */
+    if (outsource[0] === work1[attr]) return -1;
+    /* order not maintained implies positive ordering */
+    return 1;
   }
 
   Template.library_list.get_works = get_works
