00519733912e1b97fef875789af5a9862f4ded97
Ari Entlich <atrigent@gmail.com>
2014-07-13 21:51:32 +0000

    Change the way that the library page is switched between list and grid mode.
    Now all we have to do is add/remove some classes on one element. This is pretty
    much ideal.

--- client/controllers/library.js
+++ client/controllers/library.js
@@ -1,18 +1,15 @@
 if (Meteor.isClient) {
 
-  var get_works = function() {
+  Template.library.get_works = function() {
     /* TODO support {sort: [blah]} */
     return Works.find({}, { fields: { title: 1, author: 1, summary: 1 } });
   }
 
-  Template.library_list.get_works = get_works
-  Template.library_grid.get_works = get_works
-
-  Template.library.library = function() {
+  Template.library.grid_classes = function() {
     if (Session.equals('library-mode', 'grid')) {
-      return Template['library_grid'];
+      return 'small-block-grid-1 medium-block-grid-2 large-block-grid-3'
     } else  {
-      return Template['library_list'];
+      return '';
     }
   }
 
