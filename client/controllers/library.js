bab201eda1433a4d51e70fea028fe23103b8b1ea
Ari Entlich <atrigent@gmail.com>
2014-07-13 20:08:48 +0000

    Remove attempts at using Foundation's "equalizer" plugin.
    This is not designed for equalizing the sizes of things in a grid.

--- client/controllers/library.js
+++ client/controllers/library.js
@@ -8,24 +8,6 @@ if (Meteor.isClient) {
   Template.library_list.get_works = get_works
   Template.library_grid.get_works = get_works
 
-  function re_equalize(node) {
-    var eq = Foundation.libs.equalizer,
-        elem = $(node).closest('[' + eq.attr_name() + ']');
-
-    if (typeof elem.data(eq.attr_name(true) + '-init') !== 'undefined') {
-      eq.equalize(elem);
-    }
-  }
-
-  Template.library_grid.rendered = function() {
-    $(document).foundation();
-  }
-
-  /* set equalizer on grid correctly after page loads */
-  Template.work.rendered = function () {
-    re_equalize(this.firstNode);
-  };
-
   Template.library.library = function() {
     if (Session.equals('library-mode', 'grid')) {
       return Template['library_grid'];
