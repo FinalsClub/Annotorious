9457c1a96527fd7ca0ab8172a2f098526f5c236a
Bryan <btbonval@gmail.com>
2014-07-22 15:07:05 -0400

    removing debug, using context

--- client/controllers/readingview.js
+++ client/controllers/readingview.js
@@ -2,10 +2,7 @@ if (Meteor.isClient) {
 
   Template.readingview.get_section_content = function() {
     var id = new Meteor.Collection.ObjectID(Router.current().params._id);
-    var section = SectionContents.findOne({_id: id})
-    console.log(section); /* shows "undefined" */
-    var html = section.html;
-    return SectionContents.findOne({_id: id}).html;
+    return SectionContents.findOne({_id: id});
   }
 
 }
