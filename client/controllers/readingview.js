f173533823918230d2adaf3a1a34fd2ba7586b41
Bryan <btbonval@gmail.com>
2014-07-22 15:23:40 -0400

    fixed error, content displays, slight formatting.

--- client/controllers/readingview.js
+++ client/controllers/readingview.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
 
-  Template.readingview.get_section_content = function() {
+  Template.readingview.get_section = function() {
     var id = new Meteor.Collection.ObjectID(Router.current().params._id);
     return SectionContents.findOne({_id: id});
   }
