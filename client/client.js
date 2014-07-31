34f8d994fa1595f285c3565cff3e8832073b2903
Ari Entlich <atrigent@gmail.com>
2014-07-31 01:23:48 +0000

    reading view panel stubbed in place

--- client/client.js
+++ client/client.js
@@ -21,4 +21,26 @@ if (Meteor.isClient) {
     return current && current.route && current.route.name === 'readingview';
   }
 
+  function set_panel(name) {
+    if (Session.equals('reading-view-panel', name) &&
+        Session.get('reading-view-panel-visible')) {
+      Session.set('reading-view-panel-visible', false);
+    } else {
+      Session.set('reading-view-panel', name);
+      Session.set('reading-view-panel-visible', true);
+    }
+  }
+
+  Template.readingmenu.events({
+    'click #show-table-of-contents': function() {
+      set_panel('table_of_contents_panel');
+    },
+    'click #show-annotations': function() {
+      set_panel('annotation_list_panel');
+    },
+    'click #show-type-menu': function() {
+      set_panel('type_panel');
+    }
+  });
+
 }
--- client/controllers/readingview.js
+++ client/controllers/readingview.js
@@ -1,8 +1,30 @@
 if (Meteor.isClient) {
 
+  Meteor.startup(function() {
+    Session.setDefault('reading-view-panel', null);
+    Session.setDefault('reading-view-panel-visible', false);
+  });
+
   Template.readingview.get_section = function() {
     var id = new Meteor.Collection.ObjectID(Router.current().params._id);
     return SectionContents.findOne(id);
+  }
+
+  Template.readingview.get_panel_visible = function() {
+    if (Session.get('reading-view-panel-visible')) {
+      return 'show-panel';
+    } else {
+      return 'hide-panel';
+    }
+  }
+
+  Template.readingview.get_panel_contents = function() {
+    var panel = Session.get('reading-view-panel');
+    if (panel === null) {
+      return null;
+    }
+
+    return Template[panel];
   }
 
 }
