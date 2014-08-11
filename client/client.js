4eb63189cc57b5471455c77a3667e841f69e42a9
Ari Entlich <atrigent@gmail.com>
2014-08-11 05:47:08 +0000

    Dramatically reduce the number of templates in home.html by making better use of template conditionals.

--- client/client.js
+++ client/client.js
@@ -4,25 +4,23 @@ if (Meteor.isClient) {
    HOME TEMPLATE
    */
 
-  /* render different menu template for logged in or out */
-  Template.top_and_side.which_menu = function() {
-    if (Meteor.userId() === null) {
-      /* user is logged out, so change to log in */
-      return Template['signed_out_menu'];
-    } else {
-      /* user is logged in, so change to log out */
-      return Template['signed_in_menu'];
-    }
-  }
-
   Template.top_and_side.events({
     'click #menubutton': function () {
        $('#sidebar').toggleClass('open');
+    },
+    'click #show-table-of-contents': function() {
+      set_panel('table_of_contents_panel');
+    },
+    'click #show-annotations': function() {
+      set_panel('annotation_list_panel');
+    },
+    'click #show-type-menu': function() {
+      set_panel('type_panel');
     }
   });
 
   /* conditionally show the reading menu on the reading view page */
-  Template.readingmenu.show_reading_menu = function () {
+  Template.top_and_side.show_reading_menu = function () {
     var current = Router.current();
     return current && current.route && current.route.name === 'readingview';
   }
@@ -36,17 +34,5 @@ if (Meteor.isClient) {
       Session.set('reading-view-panel-visible', true);
     }
   }
-
-  Template.readingmenu.events({
-    'click #show-table-of-contents': function() {
-      set_panel('table_of_contents_panel');
-    },
-    'click #show-annotations': function() {
-      set_panel('annotation_list_panel');
-    },
-    'click #show-type-menu': function() {
-      set_panel('type_panel');
-    }
-  });
 
 }
