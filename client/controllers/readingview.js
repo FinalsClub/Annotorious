db88be9c29a366d6b8bcb970156f26cd2233362a
Ari Entlich <atrigent@gmail.com>
2014-09-24 00:25:28 +0000

    Add some special styling to the current chapter.
    This was way trickier than it should have been.

--- client/controllers/readingview.js
+++ client/controllers/readingview.js
@@ -3,9 +3,23 @@ Meteor.startup(function() {
   Session.setDefault('reading-view-panel-visible', false);
 });
 
-Template.sections.chapter_link = function() {
-  return this.content_id && Router.current().params._id !== this.content_id.toHexString();
+/* This function is weird. Here's the question it essentially answers
+ * depending on the argument:
+ *
+ * true: is this the current chapter?
+ * false: does this chapter need a link? (i.e. does it have
+ *          attached content and is NOT the current chapter)
+ *
+ * It turns out that these two questions are closely related, which is
+ * why I can implement them both like this, but I have no idea what this
+ * operation should be called. So I'm calling it "foobar". Don't judge me.
+ */
+function foobar(check) {
+  return this.content_id && (Router.current().params._id === this.content_id.toHexString()) === check;
 }
+
+Template.sections.current_chapter = _.partial(foobar, true);
+Template.sections.chapter_link = _.partial(foobar, false);
 
 Template.readingview.panel_visible = function() {
   return Session.get('reading-view-panel-visible');
